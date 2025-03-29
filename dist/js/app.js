e2dcbc8cde4545dccaee1dfba2df8b681eb47a0d
Paulina Partyka <paulinapartyka2@gmail.com>
2025-03-29 15:38:32 +0100

    add dist

---
+++ dist/js/app.js
@@ -0,0 +1,75 @@
+import PathFinder from './PathFinder.js';
+import { select } from './setting.js';
+
+const app = {
+
+  initPages: function(){
+
+    const thisApp=this;
+
+    //wszystkie strony
+    thisApp.sections=document.querySelectorAll(select.allPages);
+    //console.log(thisApp.sections);
+
+    //wszystkie btn
+    thisApp.allButton=document.querySelectorAll(select.navButton);
+
+    const idFromHash=window.location.hash.replace('#/','');
+
+    //console.log(idFromHash)
+
+    let pageMatchingHash=thisApp.sections[0].id;
+
+    //console.log(pageMatchingHash);
+
+
+    for(let section of thisApp.sections){
+      if(section.id===idFromHash){
+        pageMatchingHash=section.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let button of thisApp.allButton){
+      button.addEventListener('click', function(event){
+        const clickedElement=this;
+        event.preventDefault();
+
+        const id=clickedElement.getAttribute('href').replace('#','');
+
+        thisApp.activatePage(id);
+
+        window.location.hash='#/'+id;
+      });
+    }
+
+    window.addEventListener('hashchange', function(){
+      thisApp.initPages();
+    });
+  },
+
+  activatePage:function(sectionId){
+    const thisApp=this;
+
+    for(let section of thisApp.sections){
+      section.classList.toggle(select.classNames.noActivePage, section.id!==sectionId);
+    }
+
+  },
+
+  initPathFinder:function(){
+    this.pathFinder=new PathFinder(document.querySelector(select.finderPage));
+  },
+
+  init() {
+
+    app.initPages();
+
+    app.initPathFinder();
+  },
+
+};
+
+app.init();
---
+++ dist/js/setting.js
@@ -0,0 +1,20 @@
+export const select = {
+  allPages:'.page-section',
+  homePage:'#/about',
+  navButton:'.nav-btn',
+  finderPage:'#finder',
+  gridContainer:'.grid-container',
+  submitButton:'.btn-warning',
+  summaryModal:'#summaryModal',
+  gridItem:'.grid-item',
+
+  classNames: {
+    noActivePage:'d-none',
+    selected:'selected',
+    start:'start',
+    end:'end',
+    shortestPath:'shortestPath',
+    highlighted:'highlighted',
+    gridItem:'grid-item',
+  }
+};
